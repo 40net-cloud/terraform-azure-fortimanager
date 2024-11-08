8566a4cccabe4e472cd1ff85cf1d51f8f16e5b51
yarafe <yarafe@fortinet.com>
2024-11-08 11:38:02 +0100

    Update outputs.tf

--- modules/HA/outputs.tf
+++ modules/HA/outputs.tf
@@ -11,13 +11,13 @@
 
 output "deployment_summary" {
   value = templatefile("${path.module}/summary.tftpl", {
-    location               = var.location
-    fmg_username           = var.username
+    location                = var.location
+    fmg_username            = var.username
     fmg1_public_ip_address  = data.azurerm_public_ip.fmg1pip.ip_address
     fmg1_private_ip_address = azurerm_network_interface.fmg1ifc.private_ip_address
-	fmg2_public_ip_address  = data.azurerm_public_ip.fmg2pip.ip_address
+	  fmg2_public_ip_address  = data.azurerm_public_ip.fmg2pip.ip_address
     fmg2_private_ip_address = azurerm_network_interface.fmg2ifc.private_ip_address
-	ha_ip_address  =  var.ha_ip == "public" && length(data.azurerm_public_ip.hapip) > 0 ? data.azurerm_public_ip.hapip[0].ip_address : azurerm_network_interface.fmg1ifc.ip_configuration[1].private_ip_address
+	  ha_ip_address           =  var.ha_ip == "public" && length(data.azurerm_public_ip.hapip) > 0 ? data.azurerm_public_ip.hapip[0].ip_address : azurerm_network_interface.fmg1ifc.ip_configuration[1].private_ip_address
   })
 }
 
